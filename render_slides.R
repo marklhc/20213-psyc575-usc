d2130a34d37d551c79d29ff12e3b2a6c9d61a2fb
Hok Chio (Mark) Lai <marklhc@gmail.com>
2021-08-22 16:05:33 -0700

    Add tab for R code for week 1

---
+++ render_slides.R
@@ -0,0 +1,8 @@
+# Convert Rmd slides to HTML and PDF
+for (f in dir("slides", pattern = "*.Rmd", full.names = TRUE)) {
+  render_it = function()
+    pagedown::chrome_print(f, browser = "chromium-browser")
+  pdf_file = xfun::with_ext(f, "pdf")
+  if (!file.exists(pdf_file) || file_test("-ot", pdf_file, f))
+    render_it()
+}
